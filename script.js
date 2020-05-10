be6688d00b9138c5216ed9b3d9b25ef03e35e271
PretzelJohn <58197328+PretzelJohn@users.noreply.github.com>
2020-05-10 07:16:10 -0400

    Testing dynamic loading with cookies

--- script.js
+++ script.js
@@ -1,89 +1,83 @@
-
-function enableAllButtons(){
-	document.getElementById("time_1m").disabled = false;
-	document.getElementById("time_5m").disabled = false;
-	document.getElementById("time_15m").disabled = false;
-	document.getElementById("time_30m").disabled = false;
-	document.getElementById("time_1h").disabled = false;
-	document.getElementById("time_4h").disabled = false;
-	document.getElementById("time_1d").disabled = false;
-	document.getElementById("time_1w").disabled = false;
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*86400000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname+"="+cvalue+";"+expires+";path=/";
 }
 
-function setTimeInterval(btnId, interval){
+function getCookie(cname) {
+	var name = cname+"=";
+	var dc = decodeURIComponent(document.cookie);
+	var ca = dc.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while(c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if(c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "NULL";
+}
+
+function checkCookie(cname){
+	var x = getCookie(cname);
+	if(x == "") {
+		return false;
+	}
+	return true;
+}
+
+function addWidget(symbol, interval, container) {
+	var sym = "COINBASE:"+symbol;
+	document.getElementById(container).innerHTML = "";
+	new TradingView.widget(
+		{
+			"autosize": true,
+			"symbol": sym,
+			"interval": interval,
+			"timezone": "Etc/UTC",
+			"theme": "dark",
+			"style": "1",
+			"locale": "en",
+			"enable_publishing": false,
+			"save_image": false,
+			"container_id": container
+		}
+	);
+}
+
+function setSymbol(symbol, container) {
+	
+	
+	//set cookie
+}
+
+function setTimeInterval(interval) {
 	event.preventDefault();
-	document.getElementById("tradingview_0").innerHTML = "";
-	new TradingView.widget(
-		{
-			"autosize": true,
-			"symbol": "COINBASE:BTCUSD",
-			"interval": interval,
-			"timezone": "Etc/UTC",
-			"theme": "dark",
-			"style": "1",
-			"locale": "en",
-			"toolbar_bg": "#f1f3f6",
-			"enable_publishing": false,
-			"allow_symbol_change": true,
-			"save_image": false,
-			"container_id": "tradingview_0"
-		}
-	);
+	addWidget("BTCUSD", interval, "tradingview_0");
+	addWidget("XRPUSD", interval, "tradingview_1");
+	addWidget("XTZUSD", interval, "tradingview_2");
+	addWidget("BATUSDC", interval, "tradingview_3");
 	
-	document.getElementById("tradingview_1").innerHTML = "";
-	new TradingView.widget(
-		{
-			"autosize": true,
-			"symbol": "COINBASE:XRPUSD",
-			"interval": interval,
-			"timezone": "Etc/UTC",
-			"theme": "dark",
-			"style": "1",
-			"locale": "en",
-			"toolbar_bg": "#f1f3f6",
-			"enable_publishing": false,
-			"allow_symbol_change": true,
-			"save_image": false,
-			"container_id": "tradingview_1"
-		}
-	);
-	
-	document.getElementById("tradingview_2").innerHTML = "";
-	new TradingView.widget(
-		{
-			"autosize": true,
-			"symbol": "COINBASE:XTZUSD",
-			"interval": interval,
-			"timezone": "Etc/UTC",
-			"theme": "dark",
-			"style": "1",
-			"locale": "en",
-			"toolbar_bg": "#f1f3f6",
-			"enable_publishing": false,
-			"allow_symbol_change": true,
-			"save_image": false,
-			"container_id": "tradingview_2"
-		}
-	);
-	
-	document.getElementById("tradingview_3").innerHTML = "";
-	new TradingView.widget(
-		{
-			"autosize": true,
-			"symbol": "COINBASE:BATUSDC",
-			"interval": interval,
-			"timezone": "Etc/UTC",
-			"theme": "dark",
-			"style": "1",
-			"locale": "en",
-			"toolbar_bg": "#f1f3f6",
-			"enable_publishing": false,
-			"allow_symbol_change": true,
-			"save_image": false,
-			"container_id": "tradingview_3"
-		}
-	);
-	
-	enableAllButtons();
-	document.getElementById(btnId).disabled = true;
+	setCookie("interval", interval, 365);
+	alert("Cookie: "+getCookie("interval"));
 }
+
+function init() {
+	//read cookie
+	var interval = 15;
+	var sym0 = "BTCUSD";
+	var sym1 = "XRPUSD";
+	var sym2 = "XTZUSD";
+	var sym3 = "BATUSDC";
+	
+	
+	addWidget(sym0, interval, "tradingview_0");
+	addWidget(sym1, interval, "tradingview_1");
+	addWidget(sym2, interval, "tradingview_2");
+	addWidget(sym3, interval, "tradingview_3");
+}
+
+init();
