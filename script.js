f84863a1f4c68a38292e9f8226aff85cf45db19e
PretzelJohn <58197328+PretzelJohn@users.noreply.github.com>
2020-05-10 09:41:40 -0400

    Cookie test 1 successful... commence test 2
    Test 1 verified that cookies work once published to site. Test 2 will determine if all the controls are immediately functional, and that they save and load cookies correctly.

--- script.js
+++ script.js
@@ -1,83 +1,119 @@
-function setCookie(cname, cvalue, exdays) {
-	var d = new Date();
-	d.setTime(d.getTime() + (exdays*86400000));
-	var expires = "expires="+d.toUTCString();
-	document.cookie = cname+"="+cvalue+";"+expires+";path=/";
-}
-
-function getCookie(cname) {
-	var name = cname+"=";
-	var dc = decodeURIComponent(document.cookie);
-	var ca = dc.split(';');
-	for(var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while(c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if(c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
+class Cookie {
+	constructor(name, exdays=365) {
+		this.name = name;
+		this.exdays = exdays;
 	}
-	return "NULL";
-}
-
-function checkCookie(cname){
-	var x = getCookie(cname);
-	if(x == "") {
-		return false;
+	
+	set(value) {
+		var d = new Date();
+		d.setTime(d.getTime() + (this.exdays*86400000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = this.name+"="+this.value+";"+expires+";path=/";
 	}
-	return true;
-}
 
-function addWidget(symbol, interval, container) {
-	var sym = "COINBASE:"+symbol;
-	document.getElementById(container).innerHTML = "";
-	new TradingView.widget(
-		{
-			"autosize": true,
-			"symbol": sym,
-			"interval": interval,
-			"timezone": "Etc/UTC",
-			"theme": "dark",
-			"style": "1",
-			"locale": "en",
-			"enable_publishing": false,
-			"save_image": false,
-			"container_id": container
+	get() {
+		var name = this.name+"=";
+		var dc = decodeURIComponent(document.cookie);
+		var ca = dc.split(';');
+		for(var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while(c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if(c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
 		}
-	);
+		return "NULL";
+	}
+	
+	check(){
+		if(this.get() == "NULL") {
+			return false;
+		}
+		return true;
+	}
 }
 
-function setSymbol(symbol, container) {
+class Widget {
+	constructor(symbol, interval, container) {
+		this.symbol = symbol;		
+		this.interval = interval;
+		this.container = container;
+		this.update();
+	}
 	
+	update() {
+		var symbol = "COINBASE:"+this.symbol;
+		document.getElementById(this.container).innerHTML = "";
+		new TradingView.widget(
+			{
+				"autosize": true,
+				"symbol": symbol,
+				"interval": this.interval,
+				"timezone": "Etc/UTC",
+				"theme": "dark",
+				"style": "1",
+				"locale": "en",
+				"enable_publishing": false,
+				"save_image": false,
+				"container_id": this.container
+			}
+		);
+	}
 	
-	//set cookie
+	setSymbol(symbol) {
+		this.symbol = symbol;
+		this.update();
+	}
+	
+	setInterval(interval) {
+		this.interval = interval;
+		this.update();
+	}
 }
 
-function setTimeInterval(interval) {
+
+var intv = new Cookie("interval");
+var sym0 = new Cookie("symbol_0");
+var sym1 = new Cookie("symbol_1");
+var sym2 = new Cookie("symbol_2");
+var sym3 = new Cookie("symbol_3");
+var symbols = [sym0, sym1, sym2, sym3];
+
+function readCookie(cookie, defValue) {
+	var res = defValue;
+	if(cookie.check())
+		res = cookie.get();
+	cookie.set(res);
+	return res;
+}
+
+var interval = readCookie(intv, 15);
+
+var a = new Widget(readCookie(sym0, "BTCUSD"), interval, "tradingview_0");
+var b = new Widget(readCookie(sym1, "XRPUSD"), interval, "tradingview_1");
+var c = new Widget(readCookie(sym2, "XTZUSD"), interval, "tradingview_2");
+var d = new Widget(readCookie(sym3, "BATUSDC"), interval, "tradingview_3");
+var widgets = [a, b, c, d];
+
+function setSymbol(i, symbol) {
 	event.preventDefault();
-	addWidget("BTCUSD", interval, "tradingview_0");
-	addWidget("XRPUSD", interval, "tradingview_1");
-	addWidget("XTZUSD", interval, "tradingview_2");
-	addWidget("BATUSDC", interval, "tradingview_3");
-	
-	setCookie("interval", interval, 365);
-	alert("Cookie: "+getCookie("interval"));
+	widgets[i].setSymbol(symbol);
+	symbols[i].set(symbol);
 }
 
-function init() {
-	//read cookie
-	var interval = 15;
-	var sym0 = "BTCUSD";
-	var sym1 = "XRPUSD";
-	var sym2 = "XTZUSD";
-	var sym3 = "BATUSDC";
+function setTimeInterval(x) {
+	event.preventDefault();
 	
+	a.setInterval(x);
+	b.setInterval(x);
+	c.setInterval(x);
+	d.setInterval(x);
 	
-	addWidget(sym0, interval, "tradingview_0");
-	addWidget(sym1, interval, "tradingview_1");
-	addWidget(sym2, interval, "tradingview_2");
-	addWidget(sym3, interval, "tradingview_3");
+	intv.set(x);
 }
 
-init();
+
+
+
